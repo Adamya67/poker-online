0027b0c9207ffcda398158269333874d4fedec43
Adamya <89418564+Adamya67@users.noreply.github.com>
2026-02-14 11:21:55 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
-{
-  "name": "poker-server",
-  "version": "1.0.0",
-  "main": "index.js",
-  "type": "module",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "socket.io": "^4.7.2",
-    "cors": "^2.8.5"
-  }
-}
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Poker server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
