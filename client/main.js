d38098579e1414396b7acf5c0ce3d9f9c1ce732a
Adamya <89418564+Adamya67@users.noreply.github.com>
2026-02-13 23:24:56 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,5 +1,9 @@
 const socket = io("https://poker-online-l5i4.onrender.com");
 
 socket.on("connect", () => {
-  console.log("Connected to server");
+  console.log("Connected! My id:", socket.id);
 });
+socket.on("connect_error", (err) => {
+  console.log("Connect error:", err.message);
+});
+
